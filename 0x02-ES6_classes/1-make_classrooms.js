885e94d471a796b811c8c6ccb5a324af680cf39b
Tobi Tijani <tobi_tijani@yahoo.com>
2023-09-28 11:06:16 -0400

    ESLint corrections on Tasks 0 to 3

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const class1 = new ClassRoom(19);
-    const class2 = new ClassRoom(20);
-    const class3 = new ClassRoom(34);
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
 
-    const returnArray = [class1, class2, class3];
+  const returnArray = [class1, class2, class3];
 
-    return returnArray;
+  return returnArray;
 }
--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,69 +1,71 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-	// Check if name is a string
-	if (typeof name !== "string") {
-	    throw new TypeError("Name must be a string");
-	}
-	// Check if length is a number or greater than 0
-	if (typeof length !== "number") {
-	    throw new TypeError("Length must be a number");
-	} else if (length < 0) {
-	    throw new RangeError("Length must be greater or equal to 0");
-	}
-	// Check if students is an array of strings
-	if (!Array.isArray(students)) {
-	    throw new TypeError("Students must be an array");
-	} else {
-	    for (const student of students) {
-		if (typeof student !== "string") {
-		    throw new TypeError("Students must be an array of strings");
-		}
-	    }
-	}
-	// Now that parameter types have been verified, instantiate
-	// object properties
-	this._name = name;
-	this._length = length;
-	this._students = students;
+  constructor(name, length, students) {
+    // Check if name is a string
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    // Check if length is a number or greater than 0
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (length < 0) {
+      throw new RangeError('Length must be greater or equal to 0');
+    }
+    // Check if students is an array of strings
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      for (const student of students) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      }
+    }
+    // Now that parameter types have been verified, instantiate
+    // object properties
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    // Getter and setter for name
-    get name() {
-	return this._name;
-    }
-    set name(newName) {
-	if (typeof newName !== "string") {
-	    throw new TypeError("Name must be a string");
-	}
-	this._name = newName;
-    }
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
 
-    // Getter and setter for length
-    get length() {
-	return this._length;
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    set length(newLength) {
-	if (typeof newLength !== "number") {
-	    throw new TypeError("Length must be a number");
-	}
-	this._length = newLength;
-    }
+    this._name = newName;
+  }
 
-    // Getter and setter for students
-    get students() {
-	return this._students;
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    set students(newStudents) {
-	if (!Array.isArray(newStudents)) {
-	    throw new TypeError("Students must be an array");
-	} else {
-	    for (const student of newStudents) {
-		if (typeof student !== "string") {
-		    throw new TypeError("Students must be an array of strings");
-		}
-	    }
-	}
-	this._students = newStudents;
+    this._length = newLength;
+  }
+
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      for (const student of newStudents) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      }
     }
+    this._students = newStudents;
+  }
 }
-	    
