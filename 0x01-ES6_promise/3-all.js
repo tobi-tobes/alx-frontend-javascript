d9b3f737589fffa5978c46da6d1db223426b1881
Tobi Tijani <tobi_tijani@yahoo.com>
2023-09-29 11:38:17 -0400

    Added return statement for task 3 fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const profile = createUser();
-  Promise.all([photo, profile]).then((values) => {
+  return Promise.all([photo, profile]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
