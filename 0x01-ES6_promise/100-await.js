debbe88928ca8108741e1fe5cecfa33730fbd902
Tobi Tijani <tobi_tijani@yahoo.com>
2023-09-29 13:33:21 -0400

    Advanced task 10

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,24 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    const photoPromise = await uploadPhoto();
+    photo = photoPromise;
+  } catch (err) {
+    photo = null;
+  }
+
+  try {
+    const userPromise = await createUser();
+    user = userPromise;
+  } catch (err) {
+    user = null;
+  }
+
+  return {
+    photo,
+    user,
+  };
+}
