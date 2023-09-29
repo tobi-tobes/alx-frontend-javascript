9956fe6fe249f3eb772e976d936e6195c9f3b285
Tobi Tijani <tobi_tijani@yahoo.com>
2023-09-29 06:09:50 -0400

    task 10 fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [...array];
-  for (const element of newArray) {
+  for (const element of array) {
     const newElement = appendString + element;
-    const idx = newArray.indexOf(element);
-    newArray[idx] = newElement;
+    const idx = array.indexOf(element);
+    array[idx] = newElement;
   }
 
-  return newArray;
+  return array;
 }
