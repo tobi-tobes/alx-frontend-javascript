49d6cd8b360f7a71eaa4f7736f04d7916a072396
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-03 11:45:04 -0400

    more edge case checking for Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,25 +1,17 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string') {
+  if (!(set instanceof Set) || typeof startString !== 'string' || set.size === 0) {
     return '';
   }
   const arrayFromSet = [...set];
   const startsWithString = arrayFromSet.filter((word) => word.startsWith(startString));
 
-  if (startString.length === 0 || set.size === 0 || startsWithString.length === 0) {
+  if (startString.length === 0 || startsWithString.length === 0) {
     return '';
   }
 
-  let returnString = '';
-  let counter = 0;
+  const modifiedStrings = startsWithString.map((word) => word.replace(startString, ''));
 
-  for (const string of startsWithString) {
-    const substringRemoved = string.replace(startString, '');
-    returnString += substringRemoved;
-    if (counter < startsWithString.length - 1) {
-      returnString += '-';
-    }
-    counter += 1;
-  }
+  const returnString = modifiedStrings.join('-');
 
   return returnString;
 }
